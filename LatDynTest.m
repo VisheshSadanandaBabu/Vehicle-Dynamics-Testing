ce826ed3d0d03d4a58544b8f95538c1ab371e8b7
Vishesh Sadananda Babu <127391729+VisheshSadanandaBabu@users.noreply.github.com>
2023-12-09 09:46:35 +0100

    Update LatDynTest.m

--- LatDynTest.m
+++ LatDynTest.m
@@ -1,22 +1,29 @@
-%%Model parameters
-m = 1677;
-Vx = 18;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Author: Vishesh Sadananda Babu, Last update: 09/12/2023
+% Description:
+% * White-Box modelling to test Lateral Vehicle Dynamics of Autonomous test Vehicle
+% * MATALAB code represents an elementary 2DOF Bicycle model of an automobile which takes into account - Yaw & Lateral speed.
+% 
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-I_z = 2562; %kgm2
-l_f = 1.2; 
-l_r = 1.2;
-c_f = 260000;
-c_r = 220000;
+%% Model Parameters
+m = 1677;          % Mass of the vehicle (kg)
+V_x = 18;          % Intial velocity (m/s)
+L = 2.4;           % Distance between front and rear axles (m)
+I_z = 2562;        % Moment of inertia of the vehicle (kgm2)
+l_f = 1.2;         % Distance from centre of mass to the front axle (m)
+l_r = 1.2;         % Distance from centre of mass to the rear axle (m)
+c_f = 260000;      % Cornering stiffness of the front tire (kgm/s2)
+c_r = 220000;      % Cornering stiffness of the rear tire (kgm/s2)
 
+%% Simulink model Variables Decleration
 L =  2.4;
 Iz = 2562;
 lf = 1.2;
 lr = 1.2;
 Cf = 260000;
 Cr = 220000;
-
-% assuming constant velocity
-v_x = Vx; %mps
 
 s = tf('s');
 steering_time_constant = 0.2;
@@ -25,22 +32,23 @@ steering_tf = 1/(steering_time_constant*s + 1)^2;
 SA_new =  readmatrix('InputSA.xlsx');
 yawrate_new =  readmatrix('OrgYawRate.xlsx');
 speed_new = readmatrix('Inputspeed.xlsx');
-%% Dynamic bicycle model 
-% continous A B C D matrixes
-A = [0 1 Vx 0;
-     0 -2*(c_f+c_r)/(m*v_x) 0 -2*(l_f*c_f + l_r*c_r)/(m*v_x) - v_x;
-     0 0 0 1;
-     0 2*(l_r*c_r - l_f*c_f)/(I_z*v_x)  0  -2*(l_r^2*c_r - l_f^2*c_f)/(I_z*v_x)];
- 
-B = [0;
-    2*c_f/m;
-    0;
-    2*l_f*c_f/I_z];
 
-C= [1 0 0 0;
-       0 1 0 0];
+%% Dynamic 2DOF Bicycle model equations
+% Continous A B C D matrixes
+
+%% Matrix A: System Matrix
+A = [0 1 Vx 0; 0 -2*(c_f+c_r)/(m*v_x) 0 -2*(l_f*c_f + l_r*c_r)/(m*v_x) - v_x;
+     0 0 0 1; 0 2*(l_r*c_r - l_f*c_f)/(I_z*v_x)  0  -2*(l_r^2*c_r - l_f^2*c_f)/(I_z*v_x)];
+
+ %% Matrix B: Input Matrix
+B = [0 ; 2*c_f/m ; 0 ; 2*l_f*c_f/I_z];
+
+%% Matrix C: Lateral speed
+C= [1 0 0 0 ; 0 1 0 0];
+
+%% Matrix D: Yaw rate
 D = [0;0];
-%%
+
 vehicle = ss(A,B,C,D)
 
 %tRef = [1:0.1:327.2]';
@@ -64,7 +72,6 @@ hold on
 % xlabel('time (sec)','FontSize',11)
 % ylabel('Given yaw rate (rad/s)','FontSize',11)
 % title('Plot of the given yaw rate','FontSize',11)
-
 
 % figure(1)
 % a = out.steering_input(:,1);
@@ -134,9 +141,6 @@ title('Plot of the given yaw rate(red) and simulated yaw rate(Blue)','FontSize',
 a_3.Color = [0 0 1];
 a_5.Color = [1 0 0];
 
-
 %xRef = waypoints(:,1);
 %yRef = waypoints(:,2);
 %Ld = 10; %Lookahead distance
-
-
